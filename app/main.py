1fafdaccfae9375c41769c57ce5afc2568ac820b
amethaaa <amethajayawardena@gmail.com>
2024-03-29 17:06:55 +0530

    modify main.py

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ def remove_stopwords(response):
 def index():
     return 'Welcome to the Stress Detection API!'
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
     return Response("Hello, World!", status=200, mimetype='text/plain')
     # Get user response from request
